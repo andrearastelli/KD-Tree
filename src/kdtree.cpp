5b8ab81e7a31b287417c0d60ccaf06a1dd9b5cc3
Andrea Rastelli <rastelliandrea87@gmail.com>
2019-06-17 08:48:45 +0100

    Updated implementation

--- src/kdtree.cpp
+++ src/kdtree.cpp
@@ -1,2 +1,21 @@
 #include "kdtree.hpp"
 
+#include <cmath>
+#include <functional>
+#include <iterator>
+#include <limits>
+
+
+KDNode::KDNode() = default;
+
+KDNode::KDNode(
+    const point_t& pt, const size_t& idx_,
+    const KDNode_ptr& left_, const KDNode_ptr& right_)
+    : x{pt}, index{idx_}, left{left_}, right{right_} {}
+
+KDNode::KDNode(
+    const point_index& pi,
+    const KDnode_ptr& left_, const KDNode_ptr& right_)
+    : x{pi.first}, index{pi.second}, left{left_}, right{right_} {}
+
+KDNode::~KDNode() = default;
