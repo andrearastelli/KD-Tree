bfacb9ce734d5686213881776071877e0b1a05ac
Andrea Rastelli <rastelliandrea87@gmail.com>
2019-06-17 08:57:35 +0100

    Implemented distance squared between 2 point_t

--- src/kdtree.cpp
+++ src/kdtree.cpp
@@ -19,3 +19,26 @@ KDNode::KDNode(
     : x{pi.first}, index{pi.second}, left{left_}, right{right_} {}
 
 KDNode::~KDNode() = default;
+
+double KDNode::coord(const size_t& idx) { return x.at(idx); }
+
+KDNode::operator bool() { return !x.empty(); }
+KDNode::operator point_t() { return x; }
+KDNode::operator size_t() { return index; }
+KDNode::operator point_index() { return point_index(x, index); }
+
+KDNode_ptr NewKDNode_ptr() {
+    KDNode_ptr my_node = std::make_shared<KDNode>();
+
+    return my_node;
+}
+
+inline double dist2(const point_t& a, const point_t& b) {
+    double distc {0};
+    for (size_t i=0; i<a.size(); ++i) {
+        double di = a.at(i) - b.at(i);
+        distc += di * di;
+    }
+
+    return distc;
+}
